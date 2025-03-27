6310376d2bb1112d22fb2671a9f4446cb14a2325
Tecla Fernandes Oliveira <115437392+TeclaFernandes@users.noreply.github.com>
2025-03-27 00:02:17 -0300

    feat: estrutura principal

--- app.py
+++ app.py
@@ -19,6 +19,17 @@ def exibir_senha(event=None):
 janela = tk.Tk()
 janela.title("Gerador de Senhas")
 
+largura_janela = 200
+altura_janela = 200
+
+largura_tela = janela.winfo_screenwidth()
+altura_tela = janela.winfo_screenheight()
+
+pos_x = (largura_tela - largura_janela) // 2
+pos_y = (altura_tela - altura_janela) // 2
+
+janela.geometry(f"{largura_janela}x{altura_janela}+{pos_x}+{pos_y}")
+
 botao_gerar_senha = tk.Button(janela, text="Gerar Senha", command=exibir_senha)
 botao_gerar_senha.pack(pady=10)
 
